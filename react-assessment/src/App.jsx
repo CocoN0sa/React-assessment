ffcbcc396056a4d8ac428d9d20c9ddd3b58be661
Nosariere57 <blessingberry57@gmail.com>
2025-07-18 15:57:54 +0100

    Fix import case for Contact component in App.jsx

--- react-assessment/src/App.jsx
+++ react-assessment/src/App.jsx
@@ -2,7 +2,7 @@ import styles from "./App.module.css";
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 
 function App() {
   return (
